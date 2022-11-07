e5cba7100ce34763a75fefb9531e1db45558c919
victorlouisdg <victorlouisdg@gmail.com>
2022-11-07 11:36:28 +0100

    start for TowelFoldPlanner

--- airo_drake/planners.py
+++ airo_drake/planners.py
@@ -1,10 +1,10 @@
 from pydrake.all import AbstractValue, LeafSystem, RigidTransform, PiecewisePose, Rgba, PiecewisePolynomial
 import numpy as np
 
-from airo_drake.visualization import AddMeshcatTriad
+from airo_drake.visualization import AddMeshcatTriad, VisualizePoseTrajectory
 
 
-class Planner(LeafSystem):
+class DualArmPlannerBase(LeafSystem):
     def __init__(self, plant, meshcat):
         LeafSystem.__init__(self)
 
@@ -33,10 +33,64 @@ class Planner(LeafSystem):
         self.gripper_max_open_distance = 0.107
         self.left_gripper_traj = None
 
-    def CalcRightGripperPoseTrajectory(self, context):
-        right_X_WE_current = self.get_input_port(1).Eval(context)
+        self.DeclareInitializationDiscreteUpdateEvent(self.Initialize)
+        self.DeclarePeriodicUnrestrictedUpdateEvent(0.1, 0.0, self.Plan)
 
-        start = RigidTransform(right_X_WE_current)
+    def Initialize(self, context, state):
+        left_X_WE_current = self.get_input_port(0).Eval(context)
+        right_X_WE_current = self.get_input_port(1).Eval(context)
+        left_gripper_state_current = self.get_input_port(2).Eval(context)
+        right_gripper_state_current = self.get_input_port(3).Eval(context)
+        left_gripper_open_distance = left_gripper_state_current[0]
+        right_gripper_open_distance = right_gripper_state_current[1]
+
+        self.right_X_WE_initial = right_X_WE_current
+
+        left_X_G = {"initial": left_X_WE_current, "hold": left_X_WE_current}
+        right_X_G = {"initial": right_X_WE_current, "hold": right_X_WE_current}
+
+        # Simply hold the initialization.
+        self.left_traj_X_G = PiecewisePose.MakeLinear([0.0, 1.0], list(left_X_G.values()))
+        self.left_traj_key_poses = left_X_G
+        self.right_traj_X_G = PiecewisePose.MakeLinear([0.0, 1.0], list(right_X_G.values()))
+        self.right_traj_key_poses = right_X_G
+
+        self.left_gripper_traj = PiecewisePolynomial.ZeroOrderHold(
+            [0.0, 1.0], [[left_gripper_open_distance, left_gripper_open_distance]]
+        )
+        self.rigjt_gripper_traj = PiecewisePolynomial.ZeroOrderHold(
+            [0.0, 1.0], [[right_gripper_open_distance, right_gripper_open_distance]]
+        )
+
+    def Plan(self, context, state):
+        """Sets the trajectories for the right and left gripper poses and states."""
+        # Implement you planning here, e.g. update the trajectories.
+        self.UpdatePlanVisualization()
+
+    def UpdatePlanVisualization(self):
+        VisualizePoseTrajectory(self.meshcat, "left_trajectory", self.left_traj_X_G, self.left_traj_key_poses)
+        VisualizePoseTrajectory(self.meshcat, "right_trajectory", self.right_traj_X_G, self.right_traj_key_poses)
+
+    def OutputLeftGripperPose(self, context, output):
+        X_WE = self.left_traj_X_G.GetPose(context.get_time())
+        output.set_value(X_WE)
+
+    def OutputRightGripperPose(self, context, output):
+        X_WE = self.right_traj_X_G.GetPose(context.get_time())
+        output.set_value(X_WE)
+
+    def OutputLeftGripperPosition(self, context, output):
+        output.SetFromVector(self.left_gripper_traj.value(context.get_time()))
+
+    def OutputRightGripperPosition(self, context, output):
+        output.SetFromVector(self.left_gripper_traj.value(context.get_time()))
+
+
+class TowelFoldPlanner(DualArmPlannerBase):
+    def Plan(self, context, state):
+        initial_X_WE = self.right_X_WE_initial
+
+        start = RigidTransform(initial_X_WE)
         start.set_translation(start.translation() + [0, -0.2, 0])
         down = RigidTransform(start)
         down.set_translation(down.translation() + [0, 0, -0.1])
@@ -45,59 +99,13 @@ class Planner(LeafSystem):
         up = RigidTransform(back)
         up.set_translation(up.translation() + [0, 0, 0.1])
 
-        X_G = {"initial": right_X_WE_current, "start": start, "down": down, "back": back, "up": up}
+        X_G = {"initial": initial_X_WE, "start": start, "down": down, "back": back, "up": up}
         times = {"initial": 0.0, "start": 2.0, "down": 3.0, "back": 4.0, "up": 5.0}
 
-        for name, X in X_G.items():
-            AddMeshcatTriad(self.meshcat, f"X_G{name}", X_PT=X)
-
+        self.right_traj_key_poses = X_G
         X_G = list(X_G.values())
         times = list(times.values())
         traj_X_G = PiecewisePose.MakeLinear(times, X_G)
         self.right_traj_X_G = traj_X_G
 
-        traj_p_G = traj_X_G.get_position_trajectory()
-        p_G = traj_p_G.vector_values(traj_p_G.get_segment_times())
-
-        starts = p_G[:, :-1]
-        ends = p_G[:, 1:]
-        self.meshcat.SetLineSegments("p_G", starts, ends, 2.0, rgba=Rgba(1, 0.65, 0))
-
-    def CalculateLeftFingersTrajectory(self, context):
-        opened = self.gripper_max_open_distance
-        closed = 0.0
-        self.left_gripper_traj = PiecewisePolynomial.FirstOrderHold(
-            np.array([0.0, 2.0, 4.0, 5.0]), np.array([closed, opened, opened / 2.0, opened]).reshape([1, -1])
-        )
-
-    def OutputLeftGripperPose(self, context, output):
-        left_X_WE_current = self.get_input_port(0).Eval(context)
-        if self.inital_pose_left is None:
-            self.inital_pose_left = RigidTransform(left_X_WE_current)
-            t = self.inital_pose_left.translation() + np.array([0, -0.2, 0])
-            self.inital_pose_left.set_translation(t)
-        output.set_value(self.inital_pose_left)
-        # output.set_value(RigidTransform([-0.3, -0.3, 0.5]))
-
-    def OutputRightGripperPose(self, context, output):
-        if self.right_traj_X_G is None:
-            self.CalcRightGripperPoseTrajectory(context)
-
-        if self.right_traj_X_G is not None:
-            X_WE = self.right_traj_X_G.GetPose(context.get_time())
-            output.set_value(X_WE)
-            return
-
-        if self.inital_pose_right is None:
-            right_X_WE_current = self.get_input_port(1).Eval(context)
-            self.inital_pose_right = RigidTransform(right_X_WE_current)
-
-        output.set_value(self.inital_pose_right)
-
-    def OutputLeftGripperPosition(self, context, output):
-        if self.left_gripper_traj is None:
-            self.CalculateLeftFingersTrajectory(context)
-        output.SetFromVector(self.left_gripper_traj.value(context.get_time()))
-
-    def OutputRightGripperPosition(self, context, output):
-        output.set_value(np.array([self.gripper_max_open_distance]))
+        self.UpdatePlanVisualization()
--- airo_drake/visualization.py
+++ airo_drake/visualization.py
@@ -19,3 +19,16 @@ def AddMeshcatTriad(meshcat, path, length=0.05, radius=0.002, opacity=1.0, X_PT=
     X_TG = RigidTransform([0, 0, length / 2.0])
     meshcat.SetTransform(path + "/z-axis", X_TG)
     meshcat.SetObject(path + "/z-axis", Cylinder(radius, length), Rgba(0, 0, 1, opacity))
+
+
+def VisualizePoseTrajectory(meshcat, path, trajectory, key_poses={}):
+        traj_X_G = trajectory
+        traj_p_G = traj_X_G.get_position_trajectory()
+        p_G = traj_p_G.vector_values(traj_p_G.get_segment_times())
+
+        starts = p_G[:, :-1]
+        ends = p_G[:, 1:]
+        meshcat.SetLineSegments(path, starts, ends, 2.0, rgba=Rgba(1, 0.65, 0))
+
+        for name, X in key_poses.items():
+            AddMeshcatTriad(meshcat, f"X_G{name}", X_PT=X)
